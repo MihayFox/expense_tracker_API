9c36c4892425fd56676e5c10636b1d236f265410
Sergetec <sergetec2000@gmail.com>
2025-05-09 13:04:26 +0300

    Working logout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Root from "./components/Root"
 import Login from "./pages/Login"
 import Dashboard from './pages/Dashboard'
 import Categories from './components/Categories'
+import Products from './components/Products'
+import Logout from './components/Logout'
 
 function App() {
   return (
@@ -13,11 +15,11 @@ function App() {
         <Route path="/admin-dashboard" element={<Dashboard />}>
           <Route index element={<h1>Summary of dashboard</h1>}/>
           <Route path="categories" element={<Categories />}/>
-          <Route path="products" element={<h1>Products</h1>}/>
+          <Route path="products" element={<Products />}/>
           <Route path="orders" element={<h1>Orders</h1>}/>
           <Route path="users" element={<h1>Users</h1>}/>
           <Route path="settings" element={<h1>Settings</h1>}/>
-          <Route path="logout" element={<h1>Logout</h1>}/>
+          <Route path="logout" element={<Logout />}/>
         </Route>
         <Route path="/user-dashboard" element={<h1>User dashboard</h1>} />
         <Route path="/login" element={<Login />} />
